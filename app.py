e0b87852e8d8e346b8af9576f122d7973d94b249
Narendra Machavarapu <160421018+narendra-0821@users.noreply.github.com>
2025-04-09 22:20:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ query = st.text_input("Enter role or job description")
 
 if st.button("Get Recommendation") and query:
     try:
-        response = requests.post(
-            "http://127.0.0.1:8000/recommend",  # use your local or live backend URL
-            json={"query": query}
-        )
+        response = requests.post("https://shl-backend-rbiy.onrender.com/recommend", json={"query": query})
+
         if response.status_code == 200:
             assessments = response.json().get("recommended_assessments", [])
             for a in assessments:
